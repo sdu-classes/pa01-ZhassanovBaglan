d06459b87a51fbf0e26fc219c043d283024df8b9
ZhassanovBaglan <124437179+ZhassanovBaglan@users.noreply.github.com>
2023-02-04 17:39:50 +0600

    Create Time.java

---
+++ Time.java
@@ -0,0 +1,29 @@
+public class Time{
+  private int hour;
+  private int minute;
+  private int second;
+  
+  public Time(int h, int m, int s) {
+    this.hour = h;
+    this.minute = m;
+    this.second = s;
+  }
+  public int getHour(){
+    return this.hour;
+  }
+  public int getMinute(){
+    return this.minute;
+  }
+  public int getSecond(){
+    return this.second;
+  }
+  public void setHour(int hour) {
+    this.hour = hour;
+  }
+  public void setMinute(int minute) {
+    this.minute = minute;
+  }
+  public void setSecond(int second) {
+    this.second = second;
+  }
+}
