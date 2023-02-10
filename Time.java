cac3fb3570409062447c887e639e2713085c72ed
ZhassanovBaglan <124437179+ZhassanovBaglan@users.noreply.github.com>
2023-02-10 19:18:20 +0600

    Update Time.java

--- Time.java
+++ Time.java
@@ -1,38 +1,80 @@
-public class Time{
-  private int hour;
-  private int minute;
-  private int second;
-  
-  public Time(int h, int m, int s) {
-    this.hour = h;
-    this.minute = m;
-    this.second = s;
-  }
-  public int getHour(){
-    return this.hour;
-  }
-  public int getMinute(){
-    return this.minute;
-  }
-  public int getSecond(){
-    return this.second;
-  }
-  public void setHour(int hour) {
-    this.hour = hour;
-  }
-  public void setMinute(int minute) {
-    this.minute = minute;
-  }
-  public void setSecond(int second) {
-    this.second = second;
-  }
-  public void setTime(int h, int m, int s){
-    this.hour = h;
-    this.minute = m;
-    this.second = s;
-  }
-  public String toString(){
-    return String.format(format "%02d:%02d:%02d", hour, minute, second);
-    return hour + ":" + minute + ":" + second;
-  }
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+    Time(int hour , int minute , int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+    public int getHour(){return hour;
+    }
+    public int getMinute(){
+        return minute;
+    }
+    public int getSecond(){
+        return second;
+    }
+    public void setHour(int hour){
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+    public void setTime(int hour , int minute , int second){
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        String h = Integer.toString(getHour());
+        String m = Integer.toString(getMinute());
+        String s = Integer.toString(getSecond());
+        if(getSecond() < 10) {
+            s = "0" + s;
+        }
+        if(getMinute() < 10) {
+            m = "0" + m;
+        }
+        if(getHour() < 10) {
+            h = "0" + h;
+        }
+        return h+":"+m+":"+s;
+    }
+    public void nextSecond(){
+        if(getSecond() == 59) {
+            setSecond(0);
+            if(getMinute() == 59)
+                setMinute(0);
+            else
+                setMinute(getMinute() + 1);
+
+            if (getHour() != 23)
+                setHour(getHour() + 1);
+            else setHour(0);
+        }else
+            setSecond(1+getSecond());
+
+    }
+    public void previousSecond(){
+        if(getSecond() == 0) {
+            setSecond(59);
+            if(getMinute() == 0)
+                setMinute(59);
+            else setMinute(getMinute() - 1);
+            if(getHour() == 0)
+                setHour(23);
+            else setHour(getHour() - 1);
+        }
+        else
+            setSecond(getSecond()-1);
+
+    }
 }
