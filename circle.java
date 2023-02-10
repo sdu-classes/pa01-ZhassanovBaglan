8109942f9944a90aa7efe59174496d0d253afd13
ZhassanovBaglan <124437179+ZhassanovBaglan@users.noreply.github.com>
2023-02-10 19:11:35 +0600

    Update circle.java

--- circle.java
+++ circle.java
@@ -1,1 +1,38 @@
+package Shapes;
+class Circle extends Shape{
+    private double radius = 1.0;
 
+    public Circle(){}
+
+    public Circle(double radius){this.radius = radius;}
+
+    public Circle(double radius , String color , boolean filled){
+        super(color , filled);
+        this.radius = radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return ((int)(Math.PI * radius * radius * 100))/100.0;
+    }
+
+    public double getPerimeter(){
+        return (int)(2 * Math.PI * radius * 100) /100.0;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
